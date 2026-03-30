826cdd1d9f9184b8ad42dd6113eb61d81f7e0f48
hussinjumah-spec <hussinjumah@gmail.com>
2026-03-30 21:33:10 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,90 @@
+const videosData = [
+    {
+        id: "kumayl-new-1",
+        title: "دعاء كميل - القارئ أحمد فردان",
+        youtubeId: "F_tPaNlxLrY",
+        category: "kumayl",
+        views: "مضاف حديثاً",
+        date: ""
+    },
+    {
+        id: "kumayl-new-2",
+        title: "دعاء كميل - القارئ حسين القطان (26-03-2026)",
+        youtubeId: "iKpxXub24hE",
+        category: "kumayl",
+        views: "مضاف حديثاً",
+        date: ""
+    },
+    {
+        id: "kumayl-new-3",
+        title: "دعاء كميل - القارئ كميل عاشور (05-03-2026)",
+        youtubeId: "r-mXkGJICGU",
+        category: "kumayl",
+        views: "مضاف حديثاً",
+        date: ""
+    },
+    {
+        id: "kumayl-1",
+        title: "دعاء كميل - بصوت أباذر الحلواجي",
+        youtubeId: "vB6Y_HXXlZ4", // Example ID
+        category: "kumayl",
+        views: "1.2M مشاهدة",
+        date: "منذ سنة"
+    },
+    {
+        id: "kumayl-2",
+        title: "دعاء كميل - باسم الكربلائي",
+        youtubeId: "JmDCRNl-n2o", 
+        category: "kumayl",
+        views: "800K مشاهدة",
+        date: "منذ 3 سنوات"
+    },
+    {
+        id: "kumayl-3",
+        title: "دعاء كميل - الشيخ حسين الأكرف",
+        youtubeId: "N1oJ4o1s1-o", 
+        category: "kumayl",
+        views: "500K مشاهدة",
+        date: "منذ 5 سنوات"
+    },
+    {
+        id: "nudba-1",
+        title: "دعاء الندبة - ميثم التمار",
+        youtubeId: "rK4LwD9aF54", 
+        category: "nudba",
+        views: "900K مشاهدة",
+        date: "منذ 4 سنوات"
+    },
+    {
+        id: "nudba-2",
+        title: "دعاء الندبة - أباذر الحلواجي",
+        youtubeId: "zXF0H8pG5W8", 
+        category: "nudba",
+        views: "600K مشاهدة",
+        date: "منذ سنتين"
+    },
+    {
+        id: "general-1",
+        title: "دعاء الصباح - أمير المؤمنين (ع)",
+        youtubeId: "Fk3-tZ8N_2w", 
+        category: "general",
+        views: "2M مشاهدة",
+        date: "منذ 6 سنوات"
+    },
+    {
+        id: "general-2",
+        title: "مناجاة التائبين - الإمام السجاد (ع)",
+        youtubeId: "QpZ5j6V0Cq0", 
+        category: "general",
+        views: "300K مشاهدة",
+        date: "منذ سنة"
+    },
+    {
+        id: "general-3",
+        title: "دعاء مكارم الأخلاق",
+        youtubeId: "Yl3a9z0J4wQ", 
+        category: "general",
+        views: "150K مشاهدة",
+        date: "منذ شهرين"
+    }
+];
